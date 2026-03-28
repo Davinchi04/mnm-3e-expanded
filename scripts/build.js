a875b9dcc6560dc22f1208737b68e03effa50281
alexsaintc-dotcom <alex.saint.c@gmail.com>
2026-03-28 17:48:49 -0500

    Align special and action fields to translated values

--- scripts/build.js
+++ scripts/build.js
@@ -75,7 +75,7 @@ async function buildPowers() {
       "system": {
         "type": systemType,
         "activate": true,
-        "special": action,
+        "special": translatedAction, // Both special and action should be translated
         "action": translatedAction,
         "portee": translationMap.range[range] || 'contact',
         "duree": translationMap.duration[duration] || 'instantane',
